166f2159731a632db0e8a18aebcdada622b01d0d
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-27 12:34:12 +0700

    Update btvn8Sess6.cpp

--- btvn8Sess6.cpp
+++ btvn8Sess6.cpp
@@ -1,13 +1,15 @@
-#include<stdio.h>
+include <stdio.h>
+
 int main(){
-	float money;
-	float laiXuat;
-	int soThang;
-	
-	printf("nhap vao so tien gui:");
-	scanf("%f", &money);
-	
-	printf("nhap vao so thang gui: ");
-	scanf("%d", &soThang);
-	
+	float money,tienGui,bank;
+	int thang;
+	printf("vui long nhap vao tien gui, lai hang thang,so thang:\n");
+	scanf("%f %f %d",&bank,&tienGui,&thang);
+	money=bank;
+	while(thang>0){
+		thang--;
+		money=money*(1+0.01*tienGui);
+	}
+	printf("tienlai: %.3f\ntien nhan duoc: %.3f",money-bank,money);
+	return 0;
 }
