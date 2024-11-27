f1ea8849007420b675fddc2cc2af19724ff78074
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-27 13:20:37 +0700

    Update btvn4Sess6.cpp

--- btvn4Sess6.cpp
+++ btvn4Sess6.cpp
@@ -3,8 +3,7 @@
 int main(){
 	float x1,x2;
 	float a,b,c;
-	float delta = b*b - 4*a*c; 
-		
+	
 	printf("nhap vao so a: ");
 	scanf("%f", &a);
 	
@@ -13,17 +12,18 @@ int main(){
 	
 	printf("nhap vao so c: ");
 	scanf("%f", &c);
-	
+
+	float delta = b*b - 4*a*c; 
 	
 	printf("%.2fx^2 + %.2fx + %.2f = 0 \n gia tri cua x la : \n",a,b,c);
 	if(delta<0){
 		printf("phuong trinh vo nghiem");
-	}else if(delta=0){
+	}else if(delta==0){
 		float x1 = x2 = -b/(2*a);
 		printf("phuong trinh co nghiem kep \n%f \n%f", x1, x2);
 	}else{
-		float x1 = (-b + sqrt(delta))/2*a;
-		float x2 = (-b - sqrt(delta))/2*a;
+		float x1 = (-b + sqrt(delta))/(2*a);
+		float x2 = (-b - sqrt(delta))/(2*a);
 		printf("phuong trinh co 2 nghiem phan biet \n%f \n%f", x1, x2);
 	}
 	
